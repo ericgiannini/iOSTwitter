7c77f2b39fe0ad162ca1e14bcfbd48175834bef1
ericgiannini <eric.giannini@yahoo.com>
2015-11-15 19:40:30 -0800

    Updated User Interface for UITextView

--- twitterIntegration/ViewController.m
+++ twitterIntegration/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextView *tweetTextView;
+
+- (void)configureTweetTextView ;
+
 @end
 
 @implementation ViewController
@@ -17,11 +21,27 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    [self configureTweetTextView];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)showShareAction:(id)sender {
+}
+
+- (void)configureTweetTextView {
+    
+    self.tweetTextView.layer.backgroundColor = [UIColor colorWithRed:1.0 green: 1.0 blue:0.9 alpha:1.0].CGColor;
+    
+    self.tweetTextView.layer.cornerRadius = 10.0;
+    
+    self.tweetTextView.layer.borderColor = [UIColor colorWithWhite:0 alpha:0.5].CGColor;
+    
+    self.tweetTextView.layer.borderWidth = 2.0;
+    
 }
 
 @end
